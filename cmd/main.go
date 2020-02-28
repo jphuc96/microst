4cb7f5a3e912d75351747d6eea0e0ceb67c36f36
Hoang Phuc Nguyen <jphuc96@gmail.com>
2020-02-28 15:22:01 +0700

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"os"
+
 	"github.com/jphuc96/microst/pkg/db/mongodb"
 	"github.com/labstack/echo/v4"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 
 	"github.com/jphuc96/microst/pkg/api"
 )
 
 func main() {
 	// app.InitConfig()
-	viper.SetConfigFile("/app/microst.yaml")
+	// viper.SetConfigFile("/app/microst.yaml")
 
-	uri := viper.GetString("mongodb.uri")
+	// uri := viper.GetString("mongodb.uri")
+	uri := os.Getenv("MONGODB_URI")
 
 	_, err := mongodb.New(uri)
 	if err != nil {
